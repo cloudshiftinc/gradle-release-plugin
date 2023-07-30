ed49ff25d20c3631837fbf3a060b6eaab8635be7
Chris Lee <chris@cloudshiftconsulting.com>
2023-07-29 22:36:57 -0700

    Re-enable cache

--- .github/workflows/build.main.kts
+++ .github/workflows/build.main.kts
@@ -46,7 +46,6 @@ workflow(
                         gradleVersion = "wrapper",
                         gradleHomeCacheCleanup = true,
                         gradleHomeCacheIncludes = listOf("jdks", "caches", "notifications"),
-                        cacheDisabled = true,
                         arguments =
                             "build publishPlugins -Pgradle.publish.key=\$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=\$GRADLE_PUBLISH_SECRET --info --scan --stacktrace --no-configuration-cache"
                     )
